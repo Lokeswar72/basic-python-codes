3883296449aea18368b32943af1ac07fb2aa95c8
loki-basireddy <169087064+Lokeswar72@users.noreply.github.com>
2025-03-23 00:24:24 +0530

    loki

---
+++ codes/1.py
@@ -0,0 +1,19 @@
+"""
+Scenario: The Rectangle Riddle
+You've just started your internship at Code Wizards Inc.,
+a leading software development firm.
+On your first day, you're given a Python script that calculates the area of a rectangle.
+It's not working correctly, though.
+Your supervisor mentions there's a small error and challenges you to find and fix it.
+"""
+
+# The Code
+
+length = 5
+width = 3
+
+# Calculate the area 
+area = length * width # Modify this line of code
+
+# Print the result
+print("The area of the rectangle is:", area)
